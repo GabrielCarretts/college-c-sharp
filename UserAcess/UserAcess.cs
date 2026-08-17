6d24f01e249b211cde1c876f62b70b968c2655ff
Gabriel Carretts <gabrielcarretts@gmail.com>
2026-08-17 19:10:53 -0300

    feat: add UserAcess project with user input handling and access validation

---
+++ UserAcess/UserAcess.cs
@@ -0,0 +1,20 @@
+﻿// Carlos Gabriel Leite Carretts //
+
+Console.Write("Informe o nome: ");
+string? usuario = Console.ReadLine();
+Console.Write("Informe a idade: ");
+string auxIdade = Console.ReadLine();
+int idade = int.Parse(auxIdade);
+Console.Write("O usuário é ativo? (true/false) ");
+string auxAtivo = Console.ReadLine();
+bool ativo = bool.Parse(auxAtivo);
+
+string mensagem = (usuario, idade, ativo) switch
+{
+    (null or "", _, _) => "Acesso negado! Usuário inexistente!",
+    (_, < 18, _) => "Acesso negado! Usuário é menor de idade!",
+    (_, _, false) => "Acesso negado! Usuário não é ativo!",
+    _ => "Acesso permitido!"
+};
+
+Console.Write($"\n{mensagem}");
